c1ac4f471c4f097c4935303179a03b38e54b27f2
gimbyeongjin <kbj2043@gmail>
2014-10-25 00:07:04 +0900

    memberCallback 서비스 모듈 만듬.

--- js/controllers/editorController.js
+++ js/controllers/editorController.js
@@ -7,13 +7,14 @@ define([
     'app',
     'directives/draggable',
     'services/SavePaper',
-    'services/httpLogout'
+    'services/httpLogout',
+    'services/memberCallback'
 ], function(
     $,
     ng,
     app
     ) {
-    app.controller('editorController', ['$scope', '$http', 'httpLogout', 'serverURL', 'SavePaper', function ($scope, $http, httpLogout, serverURL, SavePaper) {
+    app.controller('editorController', ['$scope', '$http', 'httpLogout', 'serverURL', 'SavePaper', 'memberCallback', function ($scope, $http, httpLogout, serverURL, SavePaper, memberCallback) {
         //member
         $scope.errors = [];
         $scope.msgs = [];
@@ -27,42 +28,8 @@ define([
 
             var href = "index.html";
 
-            // Success
-            if (data.returnCode == '000') {
-                // 성공 메세지
-                $scope.msgs.push("성공하였습니다.");
+            memberCallback($scope, data, href);
 
-                // 에디터 화면으로 이동
-                location.href = href //"#/articleEditor";
-            }
-
-            // Invalid Arguments
-            else if (data.returnCode == '001') {
-                // 오류 발생 메세지
-                $scope.msgs.push("오류 발생");
-            }
-
-            // Not Login
-            else if (data.returnCode == '002') {
-                // 로그인 안됬다. 메세지
-                $scope.msgs.push("로그인이 안되어있습니다.");
-
-                // 로그인 시도
-            }
-
-            // Incorrect ID or PW
-            else if (data.returnCode == '101') {
-                // 잘못된 형식의 아이디이거나 패스워드라는 메세지
-                $scope.msgs.push("잘못된 형식의 아이디이거나 패스워드입니다.");
-
-            }
-
-            // Exist Email
-            else if (data.returnCode == '102') {
-                // 이미 존재하는 이메일이라는 메세지
-                $scope.msgs.push("이미 존재하는 이메일입니다.");
-
-            };
         };
 
         $scope.itemArray = [];
