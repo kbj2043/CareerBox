1c703dbd97ff3607d88a9ceac5ad44b662150bdf
정보람(24-1) <shelter.sune@secsm.org>
2014-11-06 18:04:34 +0900

    main.js commit

--- app/main.js
+++ app/main.js
@@ -12,10 +12,19 @@ require.config({
         'jquery-ui':'../libs/jquery/jquery-ui.min',
         'domReady': '../libs/require/domReady',
         'component': '../component',
-        'twitter-bootstrap':'../libs/bootstrap/bootstrap.min'
+        'twitter-bootstrap':'../libs/bootstrap/bootstrap.min',
+        'kendo.core':'../libs/kendo/src/kendo.core',
+        'kendo.userevents':'../libs/kendo/src/kendo.userevents.',
+        'kendo.draganddrop':'../libs/kendo/src/kendo.draganddrop',
+        'kendo.resizable':'../libs/kendo/src/kendo.resizable',
+        'kendo.splitter':'../libs/kendo/src/kendo.splitter'
     },
 
     shim: {
+        'kendo':{
+            deps: ['kendo.core', 'kendo.userevents', 'kendo.draganddrop', 'kendo.resizable', 'kendo.splitter'],
+            exports: 'kendo'
+        },
         'angular': {
             deps: ['jquery'],
             exports: 'angular'
@@ -42,7 +51,7 @@ require([
     'jquery'
 ], function ($) {
 
-    var cntl = $('body').attr('ng-controllers');
+    var cntl = $('body').attr('ng-controller');
 
     require([
         'bootstrap',
